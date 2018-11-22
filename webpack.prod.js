49f963fd7c6d62b58bb657dbd2ad923d5511e019
Mark24 <mark.zhangyoung@qq.com>
2018-11-22 17:36:42 +0800

    feat(stat): add stat mode

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,15 +1,32 @@
 const webpack = require("webpack");
 const merge = require('webpack-merge');
 const common = require('./webpack.common.js');
+const BundleAnalyzerPlugin = require("webpack-bundle-analyzer")
+  .BundleAnalyzerPlugin;
 
-module.exports = merge(common, {
-  mode: 'production',
+let webpackModule = merge(common, {
+  mode: "production",
   optimization: {
     runtimeChunk: "single"
   },
-  plugins:[
+  plugins: [
     new webpack.DefinePlugin({
-      'process.env.NODE_ENV': JSON.stringify('production')
+      "process.env.NODE_ENV": JSON.stringify("production")
     })
   ]
 });
+
+// 分析模式
+if(process.env.STAT==='stat') {
+  webpackModule = merge(webpackModule,{
+    plugins: [
+      new BundleAnalyzerPlugin({
+        analyzerMode: "static",
+        generateStatsFile: true,
+        openAnalyzer: true,
+      })
+    ]
+  })
+}
+
+module.exports = webpackModule;
